4f9f2afeb86436b8b81e72af825edafb291fd95c
krsamir <samir1741012321@gmail.com>
2020-06-04 14:24:30 +0530

    Hello World Program

--- Fundamentals/Hello_world.cs
+++ Fundamentals/Hello_world.cs
@@ -1,7 +1,15 @@
-class Program  
-    {  
-        static void Main(string[] args)  
-        {  
-            System.Console.WriteLine("Hello World!");  
-        }  
-    }  
+// C# program to print Hello World
+using System;
+// namespace declaration
+namespace HelloWorldApp{
+    // class Declaration
+    class Hello_world{
+        static void Main(String[] args){
+            // printing hello world
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();      // To prevents the screen from  
+            // running and closing quickly 
+            
+        }
+    }
+}
